1444dadf571497b2d3b4883e922e4df540c4612c
c40g <c40g@kk.dk>
2019-04-30 08:30:49 +0200

    changed version

--- infoblox/infoblox.py
+++ infoblox/infoblox.py
@@ -1,14 +1,15 @@
+from typing import List, Tuple
+from ipaddress import ip_network
 import requests
 import urllib3
 
-from typing import List, Tuple
-
+from infoblox.infoblox_exceptions import *
 from .speciel_characters import speciel_characters_dict
 
 
 class Infoblox:
 
-    def __init__(self, ip: str, username: str, password: str, wapi_version: str, ssl=True):
+    def __init__(self, ip: str, username: str, password: str, network_view: str, wapi_version: str, ssl=True):
         if not ssl:
             urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         self._base_url = f'https://{ip}/wapi/v{wapi_version}/'
@@ -18,6 +19,7 @@ class Infoblox:
         self._grid_id = self._get_grid_id()
         self._settings = {'cookies': self._cookie, 'verify': False}
         self._fixed_ip_cache = {}
+        self._network_view = network_view
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         requests.post(f'{self._base_url}{self._grid_id}?_function=restartservices', **self._settings)
@@ -30,10 +32,9 @@ class Infoblox:
             characters = characters.replace(letter, speciel_characters_dict[letter])
         return characters
 
-    def _get_grid_id(self) -> str:
-        r = requests.get(f'{self._base_url}grid', auth=(self._username, self._password), verify=False)
-        self._cookie = r.cookies
-        return r.json()[0]['_ref']
+    def _delete(self, url: str) -> requests:
+        r = requests.delete(f'{self._base_url}{url}', **self._settings)
+        return r
 
     def _delete_fixed_ip(self, ref: str) -> bool:
         r = requests.delete(f'{self._base_url}{ref}', **self._settings)
@@ -42,16 +43,31 @@ class Infoblox:
         else:
             return False
 
-    def close(self):
-        requests.post(f'{self._base_url}{self._grid_id}?_function=restartservices', **self._settings)
+    def _get(self, url: str, data: dict = {}) -> requests:
+        if data:
+            r = requests.get(f'{self._base_url}{url}', json=data, **self._settings)
+        else:
+            r = requests.get(f'{self._base_url}{url}', **self._settings)
+        return r
 
-    def get_fixed_ip(self, mac: str) -> Tuple[List[dict], int]:
+    def _get_grid_id(self) -> str:
+        r = requests.get(f'{self._base_url}grid', auth=(self._username, self._password), verify=False)
+        self._cookie = r.cookies
+        return r.json()[0]['_ref']
 
-        return_fields = 'ipv4addr,mac,name,comment'
-        r = requests.get(f'{self._base_url}fixedaddress?mac={mac}&_return_fields={return_fields}', **self._settings)
-        if r.status_code == 200:
-            self._fixed_ip_cache[mac] = [fixed_ip['_ref'] for fixed_ip in r.json()]
-        return (r.json(), r.status_code)
+    def _post(self, url: str, data: dict = {}) -> requests:
+        if data:
+            r = requests.post(f'{self._base_url}{url}', json=data, **self._settings)
+        else:
+            r = requests.post(f'{self._base_url}{url}', **self._settings)
+        return r
+
+    def _put(self, url: str, data: dict = {}) -> requests:
+        if data:
+            r = requests.put(f'{self._base_url}{url}', json=data, **self._settings)
+        else:
+            r = requests.put(f'{self._base_url}{url}', **self._settings)
+        return r
 
     def delete_fixed_ip(self, mac: str = '', ref: str = '') -> dict:
         deleted_fiex_ips = {}
@@ -71,25 +87,76 @@ class Infoblox:
 
         return deleted_fiex_ips
 
-    def create_fix_ip(self, ip: str, mac: str, name: str, comment: str) -> requests:
-        r = requests.post(f'{self._base_url}fixedaddress?mac={mac}&ipv4addr={ip}&name={name}&comment={comment}',
-                          **self._settings)
+    def delete_network(self, ip: str) -> requests:
+        try:
+            ref = self.get_network(ip=ip).json()[0]['_ref']
+        except IndexError:
+            raise NetworkDoesNotExist(f'Not network exist with ip:{ip}')
 
+        r = self._delete(url=ref)
+        return r
+
+    def close(self):
+        self._post(url=f'{self._grid_id}?_function=restartservices')
+
+    def create_network(self, ip: str, netmask_bits: str, comment: str, members: List[dict],
+                       options: List[dict]) -> requests:
+        data = {'network': f'{ip}/{netmask_bits}',
+                'comment': comment,
+                'network_view': self._network_view,
+                'options': options,
+                'members': members,
+                }
+        r = self._post(url=f'network', data=data)
+        return r
+
+    def create_range(self, ip: str, netmask_bits: str, member: dict) -> requests:
+        network = f'{ip}/{netmask_bits}'
+        hosts = list(ip_network(network).hosts())
+
+        data = {'network': network,
+                'network_view': self._network_view,
+                'start_addr': str(hosts[0]),
+                'end_addr': str(hosts[-1]),
+                'member': member,
+                }
+        return self._post(url='range', data=data)
+
+    def create_fix_ip(self, ip: str, mac: str, name: str, comment: str) -> requests:
+        r = self._post(url=f'fixedaddress?mac={mac}&ipv4addr={ip}&name={name}&comment={comment}')
+        return r
+
+    def get_all_networks(self, max_results: int = 20000,
+                         return_fiedls: str = 'network,netmask,ipv4addr,options') -> requests:
+        r = self._get(url=f'network?_max_results={max_results}&_return_fields={return_fiedls}')
+        return r
+
+    def get_fixed_ip(self, mac: str, return_fields: str = 'ipv4addr,mac,name,comment') -> requests:
+        r = self._get(url=f'fixedaddress?mac={mac}&_return_fields={return_fields}')
+        if r.status_code == 200:
+            self._fixed_ip_cache[mac] = [fixed_ip['_ref'] for fixed_ip in r.json()]
         return r
 
     def get_ip_info(self, ip: str) -> dict:
-        r = requests.get(f'{self._base_url}ipv4address?ip_address={ip}', **self._settings)
+        r = self._get(url=f'ipv4address?ip_address={ip}')
         data = r.json()
         if r.status_code == 200 and data:
             return data
         else:
             return {}
 
-    def get_all_networks(self, max_results: int=20000, return_fiedls: str='network,netmask,ipv4addr,options') -> dict:
-        r = requests.get(f'{self._base_url}network?_max_results={max_results}&_return_fields={return_fiedls}',
-                         **self._settings)
-        data = r.json()
-        if r.status_code == 200 and data:
-            return data
-        else:
-            return {}
+    def get_network(self, ip, return_fiedls: str = 'network,netmask,ipv4addr,options') -> requests:
+        return self._get(url=f'network?ipv4addr={ip}&_return_fields={return_fiedls}')
+
+    def update_dns(self, dns_servers: list, ref: str):
+        dns_string = ','.join(dns_servers)
+        data = {'options': [
+            {
+                "name": "domain-name-servers",
+                "num": 6,
+                "use_option": True,
+                "value": dns_string,
+                "vendor_class": "DHCP"
+            }]}
+
+        return requests.put(f'{self._base_url}{ref}', json=data, **self._settings)
---
+++ infoblox/infoblox_exceptions.py
@@ -0,0 +1,2 @@
+class NetworkDoesNotExist(Exception):
+    pass
